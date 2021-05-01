d5b580f35e223667fc363f0ec9e39fb8d228a4dd
wicekv <lukaszwickowski@wp.pl>
2021-05-01 12:46:22 +0200

    data processing backend

--- actions/api/noteActions.js
+++ actions/api/noteActions.js
@@ -1,42 +1,68 @@
 const Note = require('../../db/models/note');
 
 class NoteActions {
-    saveNote(req, res){
-
-        // const newNote = new Note({
-        //     title: 'Szybkie zakupy',
-        //     body: 'Zakupy w biedronce'
-        
-        // });
+    async saveNote(req, res){
         const title = req.body.title;
         const body = req.body.body;
 
-        newNote.save().then(() => {
-            console.log('Notatka została dodana' + title + ' tresc ' + body);
-        })
+        let note;
 
-        res.send('Strona główna działa!');
+        try{
+            note = new Note({title, body});
+            await note.save();
+        }catch(err){
+            return res.status(200).json({message: err.message})
+        }
+
+        res.status(200).json(note);
+
+        // // const newNote = new Note({
+        // //     title: 'Szybkie zakupy',
+        // //     body: 'Zakupy w biedronce'
+        
+        // // });
+
+        // newNote.save().then(() => {
+        //     console.log('Notatka została dodana' + title + ' tresc ' + body);
+        // })
+
+        // res.send('Strona główna działa!');
     }
 
-    getAllNotes(req, res) {
-        //pobieranie wszystkich notatek
-        res.send('pobieranie wszystkich notatek');
+    //pobieranie wszystkich notatek
+   async getAllNotes(req, res) {
+        const doc = await Note.find({});
+
+        res.status(201).json(doc);
     }
 
-    getNote(req, res) {
-        //pobieranie jednej notatki
-        res.send('pobieranie jednej notatki');
-    }
-
-    updateNote(req, res) {
-        //aktualizacja notatki
-        res.send('aktualizacja notatki');
-    }
-    
-    deleteNote(req, res) {
-        //usuwanie notatki
+    //pobieranie jednej notatki
+    async getNote(req, res) {
         const id = req.params.id;
-        res.send('usuwanie notatki o ID '+ id);
+        const note = await Note.findOne({_id: id});
+        res.status(200).json(note);
+    }
+
+    //aktualizacja notatki
+    async updateNote(req, res) {
+        const id = req.params.id
+        const title = req.body.title;
+        const body = req.body.body;
+
+        const note = await Note.findOne({_id: id});
+        note.title = title;
+        note.body = body;
+        await note.save();
+
+        res.status(201).json(note);
+    }
+
+    //usuwanie notatki
+    async deleteNote(req, res) {
+        const id = req.params.id;
+        await Note.deleteOne({_id: id});
+
+        res.sendStatus(204);
     }
 }
 
